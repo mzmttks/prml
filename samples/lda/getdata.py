eaeddf73519a3793e0d7f2e4a3161aeabf44392a
mzmttks <ta.mizumoto@gmail.com>
2016-08-26 01:34:18 +0900

    Bayesian Linear Regression & LDA

--- samples/lda/getdata.py
+++ samples/lda/getdata.py
@@ -5,23 +5,23 @@ import json
 
 base = "https://papers.nips.cc/book/advances-in-neural-information-processing-systems"
 
-baseno = 23
-baseye = 2010
-for offset in range(5):
-  no = str(baseno + offset)
-  year = str(baseye + offset)
-  url = "-".join([base, no, year])
+baseno = 13
+baseye = 2000
+for offset in range(16):
+    no = str(baseno + offset)
+    year = str(baseye + offset)
+    url = "-".join([base, no, year])
 
-  soup = bs4.BeautifulSoup(urllib2.urlopen(url), "lxml")
+    soup = bs4.BeautifulSoup(urllib2.urlopen(url), "lxml")
 
-  lis = soup.select(".main > ul li")
-  papers = []
-  for li in lis:
-    parsed = li.find_all("a")
-    papers.append({
-      "title": parsed[0].text,
-      "authors": [parsed[i].text for i in range(1, len(parsed))]
-    })
+    lis = soup.select(".main > ul li")
+    papers = []
+    for li in lis:
+        parsed = li.find_all("a")
+        papers.append({
+          "title": parsed[0].text,
+          "authors": [parsed[i].text for i in range(1, len(parsed))]
+        })
 
-  print year, len(papers)
-  json.dump(papers, open("nips-%s.json" % year, "w"))
+    print year, len(papers)
+    json.dump(papers, open("nips-%s.json" % year, "w"))
--- samples/lda/topicmodel.py
+++ samples/lda/topicmodel.py
@@ -1,32 +1,61 @@
 #
 # https://openbook4.me/projects/193/sections/1154
-# 
+#
 
-from gensim import corpora, models, similarities
+from gensim import corpora, models
 import json
+import wordcloud
+import numpy
+from PIL import Image
 
-documents = map(lambda d:d["title"], json.load(open("nips-2015.json")))
+for year in range(2000, 2016):
+    print year
+    documents = map(lambda d: d["title"],
+                    json.load(open("nips-%d.json" % year)))
 
-texts = [doc.lower().split() for doc in documents]
+    texts = [doc.lower().split() for doc in documents]
 
-all_tokens = sum(texts, [])
-tokens_once = set(w for w in set(all_tokens) if all_tokens.count(w) == 1)
-texts = [[w for w in text if w not in tokens_once]
-         for text in texts]
+    all_tokens = sum(texts, [])
+    tokens_once = set(w for w in set(all_tokens) if all_tokens.count(w) == -1)
+    stop_words = ["of", "for", "in", "using", "and", "to",
+                  "a", "an", "the", "via", "on", "with",
+                  "by", "as", "is", "from",
+                  "model", "models", "new"]
 
+    texts = [[w for w in text if w not in tokens_once and w not in stop_words]
+             for text in texts]
 
-dictionary = corpora.Dictionary(texts)
+    dictionary = corpora.Dictionary(texts)
+    dictionary.filter_extremes(no_below=5, no_above=0.8)
 
+    corpus = [dictionary.doc2bow(text) for text in texts]
 
-corpus = [dictionary.doc2bow(text) for text in texts]
-corpora.MmCorpus.serialize("test.mm", corpus)
+    # LDA
+    model = models.ldamodel.LdaModel(corpus, id2word=dictionary,
+                                     num_topics=10, passes=100)
+    wordses = []
+    for topic in model.show_topics(-1):
+        words = map(lambda a: (a.split("*")[1], float(a.split("*")[0])),
+                    topic[1].split(" + "))
+        # print ", ".join(sorted(words))
+        print words
+        wordses.append(words)
+    imgs = []
+    for index in range(len(wordses)):
+        wc = wordcloud.WordCloud()
+        wc.generate_from_frequencies(wordses[index])
+        imgs.append(wc.to_array())
 
-#TFIDF + LSI
-tfidf = models.TfidfModel(corpus)
-corpus_tfidf = tfidf[corpus]
-lsi = models.LsiModel(corpus_tfidf, id2word=dictionary, num_topics=2)
-corpus_lsi = lsi[corpus_tfidf]
-
-print corpus_lsi
-# LDA
-#model = ldamodel.LdaModel(bow_corpus, id2word=dictionary, num_topics=100)
+    # Construct a 2 x 5 image
+    width, height, skip = 400, 200, 30
+    img = 255 * numpy.ones(((height + skip) * 5 - skip,
+                            (width + skip) * 2 - skip, 3), numpy.uint8)
+    print img.shape
+    for row in range(5):
+        for column in range(2):
+            index = column * 5 + row
+            x = (width + skip) * column
+            y = (height + skip) * row
+            print x, y
+            img[y:(y+height), x:(x+width), :] = imgs[index]
+    Image.fromarray(img).save("pngs/words-%d.png" % year)
